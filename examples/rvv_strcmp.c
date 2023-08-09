5f901568524649768642f80f4a6bcd9b2652d415
Camel Coder <69110542+camel-cdr@users.noreply.github.com>
2023-08-09 12:00:16 +0000

    rvv_strcmp.c explicitly use unsigned
    explicitly use unsigned, instead of char, which can be either signed or unsigned, which doesn't play well with the signed vector types.
    
    Signed-off-by: Camel Coder <69110542+camel-cdr@users.noreply.github.com>

--- examples/rvv_strcmp.c
+++ examples/rvv_strcmp.c
@@ -4,24 +4,25 @@
 
 // reference https://github.com/riscv/riscv-v-spec/blob/master/example/strcmp.s
 int strcmp_vec(const char *src1, const char *src2) {
+  const unsigned char *s1 = (const void*)src1, *s2 = (const void*)src2;
   size_t vlmax = __riscv_vsetvlmax_e8m2();
   long first_set_bit = -1;
-  size_t vl, vl1;
+  size_t vl;
   while (first_set_bit < 0) {
-    vint8m2_t vec_src1 = __riscv_vle8ff_v_i8m2(src1, &vl, vlmax);
-    vint8m2_t vec_src2 = __riscv_vle8ff_v_i8m2(src2, &vl1, vlmax);
+    vuint8m2_t vec_src1 = __riscv_vle8ff_v_u8m2(s1, &vl, vlmax);
+    vuint8m2_t vec_src2 = __riscv_vle8ff_v_u8m2(s2, &vl, vl);
 
-    vbool4_t string_terminate = __riscv_vmseq_vx_i8m2_b4(vec_src1, 0, vl);
-    vbool4_t no_equal = __riscv_vmsne_vv_i8m2_b4(vec_src1, vec_src2, vl);
+    vbool4_t string_terminate = __riscv_vmseq_vx_u8m2_b4(vec_src1, 0, vl);
+    vbool4_t no_equal = __riscv_vmsne_vv_u8m2_b4(vec_src1, vec_src2, vl);
     vbool4_t vec_terminate = __riscv_vmor_mm_b4(string_terminate, no_equal, vl);
 
     first_set_bit = __riscv_vfirst_m_b4(vec_terminate, vl);
-    src1 += vl;
-    src2 += vl;
+    s1 += vl;
+    s2 += vl;
   }
-  src1 -= vl - first_set_bit;
-  src2 -= vl - first_set_bit;
-  return *src1 - *src2;
+  s1 -= vl - first_set_bit;
+  s2 -= vl - first_set_bit;
+  return *s1 - *s2;
 }
 
 int main() {
