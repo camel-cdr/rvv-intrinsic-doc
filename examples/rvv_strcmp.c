4232859e3e0d0951e027b33e744adf63eecbce90
Camel Coder <69110542+camel-cdr@users.noreply.github.com>
2023-08-09 12:04:53 +0000

    rvv_strcmp.c unify style with other examples
    Signed-off-by: Camel Coder <69110542+camel-cdr@users.noreply.github.com>

--- examples/rvv_strcmp.c
+++ examples/rvv_strcmp.c
@@ -7,8 +7,7 @@ int strcmp_vec(const char *src1, const char *src2) {
   const unsigned char *s1 = (const void*)src1, *s2 = (const void*)src2;
   size_t vlmax = __riscv_vsetvlmax_e8m2();
   long first_set_bit = -1;
-  size_t vl;
-  while (first_set_bit < 0) {
+  for (size_t vl; first_set_bit < 0; s1 += vl, s2 += vl) {
     vuint8m2_t vec_src1 = __riscv_vle8ff_v_u8m2(s1, &vl, vlmax);
     vuint8m2_t vec_src2 = __riscv_vle8ff_v_u8m2(s2, &vl, vl);
 
@@ -17,8 +16,6 @@ int strcmp_vec(const char *src1, const char *src2) {
     vbool4_t vec_terminate = __riscv_vmor_mm_b4(string_terminate, no_equal, vl);
 
     first_set_bit = __riscv_vfirst_m_b4(vec_terminate, vl);
-    s1 += vl;
-    s2 += vl;
   }
   s1 -= vl - first_set_bit;
   s2 -= vl - first_set_bit;
