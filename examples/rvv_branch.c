9aae769b846ac09e14420d56c2bbc2a847bc5c81
Camel Coder <69110542+camel-cdr@users.noreply.github.com>
2023-08-09 11:45:51 +0000

    rvv_branch.c use vf instead of vv
    Signed-off-by: Camel Coder <69110542+camel-cdr@users.noreply.github.com>

--- examples/rvv_branch.c
+++ examples/rvv_branch.c
@@ -11,7 +11,6 @@ void branch_golden(double *a, double *b, double *c, int n, double constant) {
 void branch(double *a, double *b, double *c, int n, double constant) {
   // set vlmax and initialize variables
   size_t vlmax = __riscv_vsetvlmax_e64m1();
-  vfloat64m1_t vec_zero = __riscv_vfmv_v_f_f64m1(0, vlmax);
   vfloat64m1_t vec_constant = __riscv_vfmv_v_f_f64m1(constant, vlmax);
   for (size_t vl; n > 0; n -= vl, a += vl, b += vl, c += vl) {
     vl = __riscv_vsetvl_e64m1(n);
@@ -19,7 +18,7 @@ void branch(double *a, double *b, double *c, int n, double constant) {
     vfloat64m1_t vec_a = __riscv_vle64_v_f64m1(a, vl);
     vfloat64m1_t vec_b = __riscv_vle64_v_f64m1(b, vl);
 
-    vbool64_t mask = __riscv_vmfne_vv_f64m1_b64(vec_b, vec_zero, vl);
+    vbool64_t mask = __riscv_vmfne_vf_f64m1_b64(vec_b, 0, vl);
 
     vfloat64m1_t vec_c = __riscv_vfdiv_vv_f64m1_mu(mask, /*maskedoff*/ vec_constant, vec_a, vec_b, vl);
     __riscv_vse64_v_f64m1(c, vec_c, vl);
